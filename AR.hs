aecf8a902268fb08f14d17f53f742efd3c6b35f2
Patrik Jansson <patrik.ja@gmail.com>
2013-06-18 11:10:03 +0200

    Some cleaning up of AR

--- AR.hs
+++ AR.hs
@@ -1,13 +1,14 @@
 {-# LANGUAGE FlexibleInstances #-}
 module AR where
-
+import qualified Data.Map as Map
+import Data.Map (Map, keys, elems)
 import Data.List
 import Data.Maybe
 
-type Map a b        = [(a,b)]
-
-dom                 = map fst
-rng                 = map snd
+dom :: Map a b -> [a]
+rng :: Map a b -> [b]
+dom = keys
+rng = elems
 
 a `subsetof` b      = all (`elem` b) a
 a `equal` b         = a `subsetof` b && b `subsetof` a
@@ -15,50 +16,45 @@ a `equal` b         = a `subsetof` b && b `subsetof` a
 unique xs           = xs == nub xs
 
 data Component =
-    Atomic (Map PortName Port) (Map VarName Type) [Runnable]
-    |
-    Composition (Map PortName Port) [(CompName,Component)] [Connector]
+      Atomic      (Map PortName Port) (Map VarName Type)       [Runnable]
+    | Composition (Map PortName Port) (Map CompName Component) [Connector]
 
 ports (Atomic iface _ _)        = iface
 ports (Composition iface _ _)   = iface
 
 data PortKind =
-    SenderReceiver (Map VarName Type)   
-    |
-    ClientServer (Map OpName Operation)
+      SenderReceiver (Map VarName Type)   
+    | ClientServer   (Map OpName Operation)  -- a sum (choice) of operations
     
 instance Eq PortKind where
-    SenderReceiver ts == SenderReceiver ts' 
-                    = ts `equal` ts'
-    ClientServer ops == ClientServer ops'   
-                    = ops `equal` ops'
+    SenderReceiver ts == SenderReceiver ts'  =  ts == ts'
+    ClientServer ops  == ClientServer ops'   =  ops == ops'
     _ == _          = False
 
-data Port =
-    Provided PortKind |
-    Required PortKind
+data Port = Provided PortKind
+          | Required PortKind
     deriving Eq
 
 inverse (Provided p)    = Required p
 inverse (Required p)    = Provided p
 
 data Operation =
-    Op (Map VarName ArgType) [Int]
+    Op (Map VarName ArgType) [Int]  -- a product (tuple) of arguments, set of error codes
 
 instance Eq Operation where
-    Op args errs == Op args' errs'  = args `equal` args' && errs `equal` errs'
+    Op args errs == Op args' errs'  = args == args' && errs == errs'
     
 data ArgType   = 
-    In    Type  |
-    InOut Type  |
-    Out   Type
+    In    Type
+  | InOut Type
+  | Out   Type
     deriving Eq
 
 type Connector  = (PortRef, PortRef)    
 
 data PortRef = 
-    Qual CompName PortName |
-    Ext PortName
+    Qual CompName PortName
+  | Ext PortName
     deriving Eq
 
 type PortName   = String
@@ -72,74 +68,80 @@ type Statevars  = Map VarName Value
 type Arguments  = Map VarName Value
 type Response   = Either Int (Map VarName Value)
 
-data Runnable =
-    Init Behavior
-    |
-    DataReceived PortName (Statevars -> Arguments -> Behavior)
-    |
-    OperationInvoked PortName OpName (Statevars -> Arguments -> Behavior)
-    |
-    Timing Double (Statevars -> Behavior)
+-- | Four different kinds of |Runnable|s
+data Runnable = 
+    Init Behavior    -- ^ should set up initial state etc.
+  | DataReceived     PortName         (Statevars -> Arguments -> Behavior)
+    -- ^ async. method
+  | OperationInvoked PortName OpName  (Statevars -> Arguments -> Behavior)
+    -- ^ sync. operation
+  | Timing           Double           (Statevars -> Behavior)
+    -- ^ repeated execution
 
-data Behavior =
-    Sending PortName Arguments Behavior 
-    |
-    Invoking PortName OpName Arguments (Response -> Behavior) 
-    |
-    Responding Response Behavior
-    |
-    Idling Statevars
-    |
-    Crashed
-    |
-    Comp (Map CompName Behavior)
+-- | Observable effect of a |Runnable|
+data Behavior = 
+    Sending   PortName Arguments Behavior 
+    -- ^ 
+  | Invoking  PortName OpName Arguments (Response -> Behavior) 
+    -- ^ Invoke operation (must check "port matching")
+  | Responding Response Behavior
+    -- ^ Act. on a call
+  | Idling Statevars
+  | Crashed
+  | Comp (Map CompName Behavior)
 
 data Observation =
-    Send PortRef Arguments
-    |
-    Invoke PortRef OpName Arguments
-    |
-    Respond Response
-    |
-    Tau
+    Send    PortRef Arguments
+  | Invoke  PortRef OpName Arguments
+  | Respond Response
+  | Tau
 
-update = undefined
-connections = undefined
-runnables = undefined
+-- update :: k -> v -> Map k v -> Map k v
+update       = undefined
+
+-- connections :: Component -> [Connector]
+connections  = undefined
+
+runnables    = undefined
 data Msg = Msg CompName PortName Arguments deriving Eq
 
 reduce env (bhvs, msgs, links) = 
-    [ (update c next bhvs, Msg c' p' args : msgs, links, Tau) | 
-        (c,Sending p args next) <- bhvs,
-        Qual c' p' <- connections env c p
-    ] ++
-    [ (update c (run svars args) bhvs, delete (Msg c p args) msgs, links, Tau) |
-        (c,Idling svars) <- bhvs,
-        DataReceived p run <- runnables env c,
-        Msg c1 p1 args <- msgs,
-        c == c1, p == p1
+
+    [ (update c next bhvs, Msg c' p' args : msgs, links, Tau)
+    | (c,Sending p args next) <- bhvs
+    , Qual c' p' <- connections env c p
     ] ++
     
-    [ (update c' (run svars args) bhvs, msgs, (c,c'):links, Tau) |
-        (c,Invoking p op args cont) <- bhvs,
-        (c',Idling svars) <- bhvs,
-        OperationInvoked p' op' run <- runnables env c',
-        Qual c' p' `elem` connections env c p
-    ] ++
-    [ (update c (cont resp) (update c' next bhvs), msgs, delete (c,c') links, Tau) |
-      (c,Invoking p op args cont) <- bhvs,
-      (c',Responding resp next) <- bhvs,
-      (c,c') `elem` links
+    [ (update c (run svars args) bhvs, delete (Msg c p args) msgs, links, Tau) 
+    | (c,Idling svars) <- bhvs
+    , DataReceived p run <- runnables env c
+    , Msg c1 p1 args <- msgs
+    , c == c1, p == p1
     ] ++
     
-    [ (update c next, msgs, links, Send (Ext p') args) |
-        (c,Sending p args next) <- bhvs,
-        Ext p' <- connections env c p
+    [ (update c' (run svars args) bhvs, msgs, (c,c'):links, Tau) 
+    | (c,Invoking p op args cont) <- bhvs
+    , (c',Idling svars) <- bhvs
+    , OperationInvoked p' op' run <- runnables env c'
+    , Qual c' p' `elem` connections env c p
     ] ++
-    [ (update c (Invoking p op args cont) bhvs, msgs, links, Invoke (Ext p') op args) |
-        (c,Invoking p op args cont) <- bhvs,
-        Ext p' <- connections env c p
+    
+    [ (update c (cont resp) (update c' next bhvs), msgs, delete (c,c') links, Tau) 
+    | (c,Invoking p op args cont) <- bhvs
+    , (c',Responding resp next) <- bhvs
+    , (c,c') `elem` links
     ] ++
+    
+    [ (update c next, msgs, links, Send (Ext p') args) 
+    | (c,Sending p args next) <- bhvs
+    , Ext p' <- connections env c p
+    ] ++
+
+    [ (update c (Invoking p op args cont) bhvs, msgs, links, Invoke (Ext p') op args) 
+    | (c,Invoking p op args cont) <- bhvs
+    , Ext p' <- connections env c p
+    ] ++
+
     [ ]
 
 --------------------------------------------------------------
@@ -150,8 +152,8 @@ validComp partial (Atomic iface sig runns)
                               unique int_ops &&
                               int_rcvs `subsetof` ext_rcvs && int_ops `subsetof` ext_ops &&
                               (partial || total)
-  where ext_rcvs            = [ p | (p, Provided (SenderReceiver _)) <- iface ]
-        ext_ops             = [ (p,o) | (p, Provided (ClientServer ops)) <- iface, o <- dom ops ]
+  where ext_rcvs            = [ p | (p, Provided (SenderReceiver _)) <- Map.assocs iface ]
+        ext_ops             = [ (p,o) | (p, Provided (ClientServer ops)) <- Map.assocs iface, o <- dom ops ]
         int_rcvs            = [ p | DataReceived p _ <- runns ]
         int_ops             = [ (p,o) | OperationInvoked p o _ <- runns ]
         total               = ext_rcvs `subsetof` int_rcvs && ext_ops `subsetof` int_ops
@@ -162,8 +164,10 @@ validComp partial (Composition iface comps conns)
                               all (validConn all_ports) conns &&
                               all (== 1) (map fan_out req_clisrv) &&
                               all (>= 1) (map fan_out req_sndrcv)
-  where all_ports           = [ (Qual c p, port) | (c,comp) <- comps, (p,port) <- ports comp ] ++
-                              [ (Ext p, inverse port) | (p,port) <- iface ]
+  where all_ports           = [ (Qual c p, port) 
+                              | (c,comp) <- Map.assocs comps, (p,port) <- Map.assocs $ ports comp ] ++
+                              [ (Ext p, inverse port) 
+                              | (p,port) <- Map.assocs iface ]
         req_sndrcv          = [ r | (r,Required (SenderReceiver _)) <- all_ports ]
         req_clisrv          = [ r | (r,Required (ClientServer _)) <- all_ports ]
         fan_out r           = length [ q | (p,q) <- conns, p == r ]
@@ -176,29 +180,29 @@ validConn ports (r,r')      = case (lookup r ports, lookup r' ports) of
 ------------------------------------------------------------------
 
 data Type =
-    TInt                            |
-    TBool                           |
-    TChar                           |
-    TArray  Type                    |
-    TStruct (Map FieldName Type)    |
-    TUnion  (Map TagName Type)
+    TInt                            
+  | TBool                           
+  | TChar                           
+  | TArray  Type                    
+  | TStruct (Map FieldName Type)    
+  | TUnion  (Map TagName Type)
 
 instance Eq Type where
     TInt == TInt                    = True
     TBool == TBool                  = True
     TChar == TChar                  = True
     TArray t == TArray t'           = t == t'
-    TStruct ts == TStruct ts'       = ts `equal` ts'
-    TUnion ts == TUnion ts'         = ts `equal` ts'
+    TStruct ts == TStruct ts'       = ts == ts'
+    TUnion ts == TUnion ts'         = ts == ts'
     _ == _                          = False
 
 data Value =
-    VInt Int                        |
-    VBool Bool                      |
-    VChar Char                      |
-    VArray [Value]                  |
-    VStruct (Map FieldName Value)   |
-    VUnion TagName Value
+    VInt Int                        
+  | VBool Bool                      
+  | VChar Char                      
+  | VArray [Value]                  
+  | VStruct (Map FieldName Value)   
+  | VUnion TagName Value
     deriving Eq
 
 hasType TInt (VInt _)               = True
@@ -206,7 +210,7 @@ hasType TBool (VBool _)             = True
 hasType TChar (VChar _)             = True
 hasType (TArray t) (VArray vs)      = all (hasType t) vs
 hasType (TStruct ts) (VStruct fs)   = unique (dom fs) && dom ts `equal` dom fs &&
-                                      and [ hasTypeIn ts f v | (f,v) <- fs ]
+                                      and [ hasTypeIn ts f v | (f,v) <- Map.assocs fs ]
 hasType (TUnion ts) (VUnion tag v)  = hasTypeIn ts tag v
 
-hasTypeIn ts k v                    = case lookup k ts of Just t -> hasType t v; _ -> False
+hasTypeIn ts k v                    = case Map.lookup k ts of Just t -> hasType t v; _ -> False
