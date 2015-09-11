fa90a663b2cf6ce204c5a8d7c6fa272a9dfbc6f2
Johan Nordlander <johan.nordlander@dataductus.se>
2015-09-11 16:55:45 +0200

    Rewrite using alternative ("ground") types.

--- ARSim/Speculation.hs
+++ ARSim/Speculation.hs
@@ -1,59 +1,69 @@
 
-type PortA  = ( DataElement Unqueued Int, 
-                DataElement Queued Boolean )
-type PortB  = ( ClientServerOperation (Int,String) Int, 
-                ClientServerOperation (String,String,Boolean) Double )
-type PortC  = ( DataElement Queued Double )
+type A  = ( DataElementUnqueued Int, 
+            DataElementQueued Boolean )
+type B  = ( ClientServerOperation (Int,String) Int, 
+            ClientServerOperation (String,String,Boolean) Double )
+type C  = ( DataElementQueued Double )
 
-comp1 :: (Required PortA, Required PortB)
+comp1 :: AR c (RPort () A, RPort () B)
 comp1 = atomic $ do
     a@(x, y) <- required (UnqueuedReceiverComSpec{init=0}, QueuedReceiverComSpec{length=32})
     b@(m, n) <- required (ClientComSpec{}, ClientComSpec{})
-    runnable [DataReceivedEvent x] $ do
-        ...
-    runnable [DataReceivedEvent y]$ do
-        ...
+    runnable [DataReceivedEvent y] $ do
+        v <- rte_Read    x
+        w <- rte_Receive y
+        r <- rte_Call    m (v,"")
+        s <- rte_Call    n ("a","b",w)
+        return ()
     return (a, b)
 
-comp2 :: (Required PortA, Provided PortB, Provided PortC)
+comp2 :: AR c (RPort () A, PPort () B, PPort () C)
 comp2 = atomic $ do
     a@(x, y) <- required (UnqueuedReceiverComSpec{init=7}, QueuedReceiverComSpec{length=8})
     b@(m, n) <- provided (ServerComSpec{length=10}, ServerComSpec{length=2})
     c@(z) <- provided (QueuedSenderComSpec{init=3.14})
-    runnable [DataReceivedEventy] $ do
-        ...
     serverRunnable [OperationInvokedEvent m] $ \(i,s) -> do
-        ...
+        v <- rte_Read a x
+        _ <- rte_Send b z (v/i)
+        return (2*v/i)
     serverRunnable [OperationInvokedEvent n] $ \(s,r,f) -> do
         ...
     return (a, b, c)
     
-comp3 :: (Required PortA, Provided PortC)
+comp3 :: AR c (RPort () A, PPort () C)
 comp3 = composition $ do
     (a1, b1) <- comp1
     (a2, b2, c2) <- comp2
     connect b1 b2
     a <- delegate [a1, a2]
-    c <- delegate [c2]
-    return (a, c)
+    return (a, c2)
 
 --------------
 
-connect :: Interface a => RPort a -> PPort a -> AR c ()
+required    :: (UnqueuedReceiverComSpec a, QueuedReceiverComSpec b) -> 
+                AR c (RPort c (DataElementUnqueued a, DataElementQueued b))
+required    :: (ClientComSpec a1 b1, ClientComSpec a2 b2) ->
+                AR c (RPort c (ClientServerOperation a1 b1, ClientServerOperation a2 b2))
 
-delegate :: Interface a => [RPort a] -> AR c (RPort a)
-delegate :: Interface a => [PPort a] -> AR c (PPort a)
+provided    :: (ServerComSpec a1 b1, ServerComSpec a2 b2) ->
+                AR c (PPort c (ClientServerOperation a1 b1, ClientServerOperation a2 b2))
+provided    :: (QueuedSenderComSpec a) ->
+                AR c (PPort c (DataElementQueued a))
 
-instance Interface (DataElement q1 a1)
-instance Interface (DataElement q1 a1, DataElement q2 a2)
-instance Interface (DataElement q1 a1, DataElement q2 a2, DataElement q3 a3)
-...
-instance Interface (ClientServerOperation a1 r1)
-instance Interface (ClientServerOperation a1 r1, ClientServerOperation a2 r2)
-instance Interface (ClientServerOperation a1 r1, ClientServerOperation a2 r2, ClientServerOperation a3 r3)
-...
+atomic      :: (forall c . AR c (RPort c A, RPort c B))            -> AR c' (RPort () A, RPort () B)
+atomic      :: (forall c . AR c (RPort c A, PPort c B), PPort c C) -> AR c' (RPort () A, PPort () B, PPort () C)
 
-required :: Interface a => RPortComSpec a -> AR c (RPort a)
+composition :: (forall c . AR c a) -> AR c' a
 
-provided :: Interface a => PPortComSpec a -> AR c (PPort a)
+connect     :: RPort () B -> PPort () B -> AR c ()
 
+delegate    :: [RPort () A] -> AR c (RPort () A)
+delegate    :: [PPort () C] -> AR c (PPort () C)
+
+rte_Read    :: RPort c (DataElementUnqueued a) -> RTE c a
+rte_Receive :: RPort c (DataElementQueued a) -> RTE c a
+rte_Send    :: PPort c (DataElementQueued a) -> a -> RTE c ()
+rte_Call    :: RPort c (ClientServerOperation a b) -> a -> RTE c b
+
+runnable        :: [Event           c] ->       RTE c b  -> AR c ()
+serverRunnable  :: [ServerEvent a b c] -> (a -> RTE c b) -> AR c ()
