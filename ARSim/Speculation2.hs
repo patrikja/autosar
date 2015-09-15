a2abb699459a16cb55c5fae01f2cfbc575889c9a
Johan Nordlander <johan.nordlander@dataductus.se>
2015-09-15 15:46:29 +0200

    More details.

--- ARSim/Speculation2.hs
+++ ARSim/Speculation2.hs
@@ -1,45 +1,7 @@
-{-# LANGUAGE MultiParamTypeClasses, FlexibleInstances, FunctionalDependencies, RankNTypes, RecordWildCards #-}
+{-# LANGUAGE MultiParamTypeClasses, FlexibleInstances, FunctionalDependencies, RankNTypes #-}
+{-# LANGUAGE RecordWildCards, ExistentialQuantification #-}
 module Speculation2 where 
-{-
-type A  = DataElementUnqueued Int
-type B  = ClientServerOperation (Int,String) Int
-type C  = DataElementQueued Double
 
-comp1 :: AR c (RPort () A, RPort () B)
-comp1 = atomic $ do
-    x <- required UnqueuedReceiverComSpec{init=0}
-    m <- required ClientComSpec{}
-    runnable [DataReceivedEvent x] $ do
-        v <- rte_Read    x
-        r <- rte_Call    m (v,"")
-        return ()
-    return (x, m)
-
-comp2 :: AR c (RPort () A, PPort () B, PPort () C)
-comp2 = atomic $ do
-    x <- required UnqueuedReceiverComSpec{init=7}
-    m <- provided ServerComSpec{length=10}
-    z <- provided QueuedSenderComSpec{init=3.14}
-    serverRunnable [OperationInvokedEvent m] $ \(i,s) -> do
-        v <- rte_Read a x
-        _ <- rte_Send b z (v/i)
-        return (2*v/i)
-    return (x, m, z)
-    
-comp3 :: AR c (RPort () A, PPort () C)
-comp3 = composition $ do
-    (a1, b1) <- comp1
-    (a2, b2, c2) <- comp2
-    connect b1 b2
-    a <- delegate [a1, a2]
-    return (a, c2)
-
---------------
-
-runnable        :: [Event           c] ->       RTE c b  -> AR c ()
-serverRunnable  :: [ServerEvent a b c] -> (a -> RTE c b) -> AR c ()
-
--}
 
 newtype DataElement q a r c             = El Int
 newtype ClientServerOperation a b r c   = Op Int
@@ -70,33 +32,45 @@ rte_Write   = undefined
 rte_Send    = undefined
 rte_Call    = undefined
 
-runnable        :: [Event c]            ->       RTE c b  -> AR c ()
-serverRunnable  :: [ ServerEvent a b c] -> (a -> RTE c b) -> AR c ()
+runnable        :: [Event c]            ->       RTE c b  -> Atomic c ()
+serverRunnable  :: [ ServerEvent a b c] -> (a -> RTE c b) -> Atomic c ()
 runnable = undefined
 serverRunnable = undefined
 
-atomic          :: (forall c . AR c (p c)) -> AR c' (p ())
+atomic          :: (forall c . Atomic c (p c)) -> AR (p ())
 atomic = undefined
 
+composition     :: AR a -> AR a
+composition = undefined
 
-data Event c            = DataReceivedEvent (forall q a . DataElement q a Required c)
+data Event c            = forall q a . DataReceivedEvent (DataElement q a Required c)
 data ServerEvent a b c  = OperationInvokedEvent (ClientServerOperation a b Provided c)
 
-data AR c a
+data Atomic c a
 
-instance Functor (AR c) where
+instance Functor (Atomic c) where
     fmap = undefined
-instance Applicative (AR c) where
+instance Applicative (Atomic c) where
     pure = undefined
     (<*>) = undefined
-instance Monad (AR c) where
+instance Monad (Atomic c) where
+    (>>=) = undefined
+
+data AR a
+
+instance Functor AR where
+    fmap = undefined
+instance Applicative AR where
+    pure = undefined
+    (<*>) = undefined
+instance Monad AR where
     (>>=) = undefined
 
 data UnqueuedSenderComSpec a    = UnqueuedSenderComSpec { initial :: a }
 data UnqueuedReceiverComSpec a  = UnqueuedReceiverComSpec { init :: a }
 data QueuedSenderComSpec a      = QueuedSenderComSpec
 data QueuedReceiverComSpec a    = QueuedReceiverComSpec { length :: Int }
-data ServerComSpec a b
+data ServerComSpec a b          = ServerComSpec { len :: Int }
 data ClientComSpec              = ClientComSpec
 
 class Interface p where
@@ -107,10 +81,12 @@ instance Interface (DataElement q a)
 instance Interface (ClientServerOperation a b)
 
 class Interface p => Port p pspec rspec | p -> pspec, p -> rspec, p pspec -> rspec, p rspec -> pspec where
-    connect :: p Provided () -> p Required () -> AR c ()
+    connect :: p Provided () -> p Required () -> AR ()
+    delegate :: [p r ()] -> AR (p r ())
     connect = undefined
-    require :: rspec -> AR c (p Required c)
-    provide :: pspec -> AR c (p Provided c)
+    delegate = undefined
+    require :: rspec -> Atomic c (p Required c)
+    provide :: pspec -> Atomic c (p Provided c)
     require = undefined
     provide = undefined
 
@@ -136,17 +112,45 @@ type Dump r c = DataElement Queued Int r c
 
 data Comp1 r c = Comp1 { portA :: PP Required c, portB :: Serv Required c }
 
+instance Interface Comp1 where
+    seal x = Comp1 { portA = seal (portA x), portB = seal (portB x) }
+
 data Comp2 r c = Comp2 { port1 :: PP Required c, port2 :: Serv Provided c, port3 :: Dump Provided c}
 
-{-
-comp1 :: AR c (Comp1 r ())
+instance Interface Comp2 where
+    seal x = Comp2 { port1 = seal (port1 x), port2 = seal (port2 x), port3 = seal (port3 x) }
+
+
+comp1 :: AR (Comp1 r ())
 comp1 = atomic $ do
-    port1 <- require (UnqueuedReceiverComSpec{init=0}, QueuedReceiverComSpec{length=10})
-    port2 <- require ClientComSpec
-{-    runnable [DataReceivedEvent x] $ do
-        v <- rte_Read    x
-        r <- rte_Call    m (v,"")
+    portA <- require (UnqueuedReceiverComSpec{init=0}, QueuedReceiverComSpec{length=10})
+    portB <- require ClientComSpec
+    runnable [DataReceivedEvent (e1 portA)] $ do
+        v <- rte_Read (e1 portA)
+        r <- rte_Call portB (23, "hello")
         return ()
--}
     return Comp1 {..}
--}
+
+comp2 :: AR (Comp2 r ())
+comp2 = atomic $ do
+    port1 <- require (UnqueuedReceiverComSpec{init=0}, QueuedReceiverComSpec{length=10})
+    port2 <- provide ServerComSpec{len=10}
+    port3 <- provide QueuedSenderComSpec
+    serverRunnable [OperationInvokedEvent port2] $ \(i,s) -> do
+        v <- rte_Read (e1 port1)
+        _ <- rte_Send port3 i
+        return (i > 0)
+    return Comp2 {..}
+
+data Comp3 r c = Comp3 { inport :: PP Required c, outport :: Dump Provided c }
+
+instance Interface Comp3 where
+    seal x = Comp3 { inport = seal (inport x), outport = seal (outport x) }
+
+comp3 :: AR (Comp3 r ())
+comp3 = composition $ do
+    c1 <- comp1
+    c2 <- comp2
+    connect (port2 c2) (portB c1)
+    inport <- delegate [portA c1, port1 c2]
+    return Comp3 { inport = inport, outport = port3 c2 }
