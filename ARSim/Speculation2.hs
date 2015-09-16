913960fbd382d1274bfaa634dabbc0f95ed80691
Patrik Jansson <patrik.ja@gmail.com>
2015-09-16 11:52:15 +0200

    Some formatting

--- ARSim/Speculation2.hs
+++ ARSim/Speculation2.hs
@@ -1,7 +1,7 @@
-{-# LANGUAGE MultiParamTypeClasses, FlexibleInstances, FunctionalDependencies, RankNTypes #-}
-{-# LANGUAGE RecordWildCards, ExistentialQuantification #-}
-module Speculation2 where 
-
+{-# LANGUAGE MultiParamTypeClasses, FunctionalDependencies, FlexibleInstances #-}
+{-# LANGUAGE ExistentialQuantification, Rank2Types #-}
+{-# LANGUAGE RecordWildCards #-}
+module Speculation2 where
 
 newtype DataElement q a r c             = El Int
 newtype ClientServerOperation a b r c   = Op Int
@@ -21,10 +21,10 @@ instance Applicative (RTE c) where
 instance Monad (RTE c) where
     (>>=) = undefined
 
-rte_Read    :: DataElement Unqueued a Required c -> RTE c a
-rte_Receive :: DataElement Queued a Required c -> RTE c a
+rte_Read    :: DataElement Unqueued a Required c ->      RTE c a
+rte_Receive :: DataElement Queued   a Required c ->      RTE c a
 rte_Write   :: DataElement Unqueued a Provided c -> a -> RTE c ()
-rte_Send    :: DataElement Queued a Provided c -> a -> RTE c ()
+rte_Send    :: DataElement Queued   a Provided c -> a -> RTE c ()
 rte_Call    :: ClientServerOperation a b Required c -> a -> RTE c b
 rte_Read    = undefined
 rte_Receive = undefined
@@ -68,42 +68,43 @@ instance Applicative Component where
 instance Monad Component where
     (>>=) = undefined
 
-data UnqueuedSenderComSpec a    = UnqueuedSenderComSpec { initial :: a }
-data UnqueuedReceiverComSpec a  = UnqueuedReceiverComSpec { init :: a }
+data UnqueuedSenderComSpec a    = UnqueuedSenderComSpec   { initial :: a }
+data UnqueuedReceiverComSpec a  = UnqueuedReceiverComSpec { init    :: a }
 data QueuedSenderComSpec a      = QueuedSenderComSpec
-data QueuedReceiverComSpec a    = QueuedReceiverComSpec { length :: Int }
-data ServerComSpec a b          = ServerComSpec { len :: Int }
+data QueuedReceiverComSpec a    = QueuedReceiverComSpec   { length  :: Int }
+data ServerComSpec a b          = ServerComSpec           { len     :: Int }
 data ClientComSpec              = ClientComSpec
 
 class Interface p where
-    seal    :: p r c -> p r ()
+    seal :: p r c -> p r ()
     seal = undefined
 
 instance Interface (DataElement q a)
 instance Interface (ClientServerOperation a b)
 
 class Interface p => Port p pspec rspec | p -> pspec, p -> rspec where
-    connect :: p Provided () -> p Required () -> Component ()
+    connect  :: p Provided () -> p Required () -> Component ()
     delegate :: [p r ()] -> Component (p r ())
-    connect = undefined
+    connect  = undefined
     delegate = undefined
     require :: rspec -> Atomic c (p Required c)
     provide :: pspec -> Atomic c (p Provided c)
     require = undefined
     provide = undefined
 
-instance Port (DataElement Unqueued a) (UnqueuedSenderComSpec a) (UnqueuedReceiverComSpec a)
-instance Port (DataElement Queued a) (QueuedSenderComSpec a) (QueuedReceiverComSpec a)
-instance Port (ClientServerOperation a b) (ServerComSpec a b) ClientComSpec
+instance Port (DataElement Unqueued a)    (UnqueuedSenderComSpec a) (UnqueuedReceiverComSpec a)
+instance Port (DataElement Queued   a)    (QueuedSenderComSpec   a) (QueuedReceiverComSpec   a)
+instance Port (ClientServerOperation a b) (ServerComSpec a b)        ClientComSpec
 
 -----------
 
-data MyPort r c = MyPort { e1 :: DataElement Unqueued Int r c, e2 :: DataElement Queued String r c }
+data MyPort r c = MyPort { e1 :: DataElement Unqueued Int    r c,
+                           e2 :: DataElement Queued   String r c }
 
 instance Interface MyPort where
     seal p = MyPort { e1 = seal (e1 p), e2 = seal (e2 p) }
 
-instance Port MyPort (UnqueuedSenderComSpec Int, QueuedSenderComSpec String) 
+instance Port MyPort (UnqueuedSenderComSpec   Int, QueuedSenderComSpec   String)
                      (UnqueuedReceiverComSpec Int, QueuedReceiverComSpec String) where
     connect a b = do connect (e1 a) (e1 b); connect (e2 a) (e2 b)
     require (spec1,spec2) = do e1 <- require spec1; e2 <- require spec2; return MyPort {..}
@@ -112,12 +113,15 @@ instance Port MyPort (UnqueuedSenderComSpec Int, QueuedSenderComSpec String)
 type Serv r c = ClientServerOperation (Int,String) Bool r c
 type Dump r c = DataElement Queued Int r c
 
-data IFace1 r c = IFace1 { portA :: MyPort Required c, portB :: Serv Required c }
+data IFace1 r c = IFace1 { portA :: MyPort Required c,
+                           portB :: Serv   Required c }
 
 instance Interface IFace1 where
     seal x = IFace1 { portA = seal (portA x), portB = seal (portB x) }
 
-data IFace2 r c = IFace2 { port1 :: MyPort Required c, port2 :: Serv Provided c, port3 :: Dump Provided c}
+data IFace2 r c = IFace2 { port1 :: MyPort Required c,
+                           port2 :: Serv   Provided c,
+                           port3 :: Dump   Provided c}
 
 instance Interface IFace2 where
     seal x = IFace2 { port1 = seal (port1 x), port2 = seal (port2 x), port3 = seal (port3 x) }
