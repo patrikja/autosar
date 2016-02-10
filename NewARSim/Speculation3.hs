ff91278ff84a1f93d57d5ae5ce546b734da4b59b
Johan Nordlander <johan.nordlander@dataductus.se>
2016-02-10 18:14:25 +0100

    More type family speculation.

--- NewARSim/Speculation3.hs
+++ NewARSim/Speculation3.hs
@@ -1,65 +1,85 @@
-{-# LANGUAGE MultiParamTypeClasses, TypeFamilies #-}
-{-# LANGUAGE RankNTypes, ExistentialQuantification #-}
-{-# LANGUAGE UndecidableInstances, FlexibleInstances, RecordWildCards #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE FlexibleInstances #-}
+module Speculation3 where
+import Unsafe.Coerce
 
-type Sealed = ()
-data DataElement c
+newtype DataElement c = DE' Int
+newtype DataElem      = DE  Int
+
+data Atomic c a
+
+instance Functor (Atomic c) where fmap = undefined
+instance Applicative (Atomic c) where pure = undefined; (<*>) = undefined
+instance Monad (Atomic c) where (>>=) = undefined
+
+data Comp a
+
+instance Functor Comp where fmap = undefined
+instance Applicative Comp where pure = undefined; (<*>) = undefined
+instance Monad Comp where (>>=) = undefined
+
+delem :: Atomic c (DataElement c)
+delem = undefined
+connect :: DataElem -> DataElem -> Comp ()
+connect = undefined
+
+atomic :: (forall c . Atomic c a) -> Comp a
+atomic = undefined
+
+--------------------
+
+type family Seal a where
+    Seal (DataElement c) = DataElem
+    Seal [a]             = [Seal a]
+    Seal (a,b)           = (Seal a, Seal b)
+    Seal (m a)           = m (Seal a)
+    Seal a               = a
 
 class Interface a where
-  type Seal a
-  seal :: a -> Seal a
+    seal :: a -> Seal a
+    seal = Unsafe.Coerce.unsafeCoerce
 
-instance Interface (DataElement c) where
-  type Seal (DataElement c) = DataElement Sealed
-  seal = undefined  -- coerce
+instance Interface a
+    
+type family Unseal a where
+    Unseal (a->b) = Seal a -> Unseal b
+    Unseal a      = a
+    
+class Sealer a where
+    fseal :: Unseal a -> a
+    fseal = undefined
 
-instance (Interface a, C b) => C (a -> b) where
-  type T (a->b) = Seal a -> T b
-  wrap f a = wrap (f (seal a))
+instance (Interface a, Sealer b) => Sealer (a -> b) where
+    fseal f a = fseal (f (seal a))
 
-data Queued         -- Parameter q above
-data Unqueued
+instance {-# OVERLAPPABLE #-} (Unseal a ~ a) => Sealer a where
+    fseal = id
 
-type DEUI = DataElement Sealed
-type DEQS = DataElement Sealed
-data MyPort r = MyPort { e1' :: DEUI,
-                         e2' :: DEQS}
+----------------------
 
-class C a where
-  type T a
-  wrap :: T a -> a
-
--- instance Eq a => C a where
-instance C Bool where
-  type T Bool = Bool
-  wrap = id
-
-instance C (a,b) where
-  type T (a,b) = (a, b)
-  wrap = id
-
-instance C (MyPort r) where
-  type T (MyPort r) = MyPort r
-  wrap = id
-
-test1 :: (DEUI, DEQS)
-test1 = wrap (,) e1 e2
-
-test2 :: MyPort r
-test2 = wrap MyPort e1 e2
-
-e1 :: DataElement c1
-e1 = undefined
-
-e2 :: DataElement c2
-e2 = undefined
+-- MyPort       :: DataElem -> [DataElem] -> MyPort
+--
+-- instance Sealer (DataElement c1 -> [DataElement c2] -> MyPort) where
+--    wrap :: Sealer (DataElement c1 -> [DataElement c2] -> MyPort) -> (DataElement c1 -> [DataElement c2] -> MyPort)
+--          ~ (Seal (DataElement c1) -> Seal [DataElement c2] -> MyPort) -> (DataElement c1 -> [DataElement c2] -> MyPort)
+--          ~ (DataElem -> [Seal (DataElement c2)] -> MyPort) -> (DataElement c1 -> [DataElement c2] -> MyPort)
+--          ~ (DataElem -> [DataElem] -> MyPort) -> DataElement c1 -> [DataElement c2] -> MyPort
 
 
+data MyPort = MyPort { e1 :: DataElem,
+                       e2 :: [DataElem] }
+--    deriving Interface
 
+comp1 :: t -> Comp (MyPort, DataElem, Maybe Char)
+comp1 x = atomic $ do 
+            a <- delem
+            b <- delem
+            x <- delem
+            return $ (fseal MyPort a [b,x], seal x, Nothing)
 
-{-
-type Serv r c   = ClientServerOperation (Int,String) Bool r c
+comp2 x = do
+    (MyPort a (b:_), x, _) <- comp1 ()
+    connect a b
+    return False
 
-data IFace1 c   = IFace1 { portA :: MyPort Required c,
-                           portB :: Serv   Required c }
--}
